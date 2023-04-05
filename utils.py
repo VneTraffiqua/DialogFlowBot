c489ad8e85c36e017f261122967cdb35eaac4b91
VneTraffiqua <safe72again@gmail.com>
2023-04-05 05:26:09 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,6 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     Using the same `session_id` between requests allows continuation
     of the conversation."""
 
-
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
@@ -31,7 +30,6 @@ if __name__ == '__main__':
     env.read_env()
     project_id = env.str('PROJECT_ID')
     session_id = env.str('DIALOGFLOW_TOKEN')
-    texts = ['хала']
     language_code = 'ru-RU'
 
     detect_intent_texts(project_id, session_id, texts, language_code)
